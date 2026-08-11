b3acf4cdfa9e1b586cec625aa7c663853bab2712
Ankitbhalke137 <bhalkeankit@gmail.com>
2026-08-11 09:08:11 +0530

    fix: restore APPS_SCRIPT_URL so transactions load

--- config.local.js
+++ config.local.js
@@ -1,1 +1,5 @@
-const CONFIG = { ALLOWED_EMAILS: ["bhalkeankit@gmail.com"] };
+const CONFIG = {
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyXE84GFzcSnw_fZeLq-_DxBaAP-5QaFlFvdC4mj1a4X-brcploZt5On1QSAPNgfCDIyg/exec',
+    GOOGLE_CLIENT_ID: '925818454032-ntv3o38j9as920n8c2t90rofd9s0hb54.apps.googleusercontent.com',
+    ALLOWED_EMAILS: ['bhalkeankit@gmail.com']
+};
